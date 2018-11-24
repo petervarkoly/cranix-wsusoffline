5b7754d3960082b3acb6d354bc0cb48923a7b835
Peter Varkoly <peter@varkoly.de>
2018-11-24 23:15:36 +0100

    Use http instead of https.

--- add-oss-wsusoffline-repo.sh
+++ add-oss-wsusoffline-repo.sh
@@ -8,13 +8,13 @@ echo "[oss-wsusoffline]
 name=oss-wsusoffline
 enabled=1
 autorefresh=1
-baseurl=https://$REPO_USER:$REPO_PASSWORD@repo.openschoolserver.net/addons/oss-wsusoffline
+baseurl=http://$REPO_USER:$REPO_PASSWORD@repo.openschoolserver.net/addons/oss-wsusoffline
 path=/
 type=rpm-md
 keeppackages=0
 " > /tmp/oss-wsusoffline.repo
 
-wget -O repomd.xml.key https://$REPO_USER:$REPO_PASSWORD@repo.openschoolserver.net/addons/oss-wsusoffline/repodata/repomd.xml.key
+wget --no-check-certificate -O repomd.xml.key https://$REPO_USER:$REPO_PASSWORD@repo.openschoolserver.net/addons/oss-wsusoffline/repodata/repomd.xml.key
 gpg --import repomd.xml.key
 zypper ar /tmp/oss-wsusoffline.repo
 zypper --gpg-auto-import-keys ref
